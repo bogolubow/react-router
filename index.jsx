c6a7fbe35518c0fc35be2af34db962fbefe1af03
Mateusz Bogolubow <mateusz@bogolubow.com>
2017-06-26 21:17:52 +0200

    Hello World - configuration test

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, Link, IndexLink, IndexRoute, hashHistory} from 'react-router';
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    ReactDOM.render(
+        <h1>Hello World</h1>,
+        document.getElementById('app')
+    );
+});
