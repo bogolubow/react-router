f91fcb3e61a59100f3b80d72c5d8f471fdf97fd5
Mateusz Bogolubow <mateusz@bogolubow.com>
2017-06-26 21:21:34 +0200

    create basic components

--- index.jsx
+++ index.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, Link, IndexLink, IndexRoute, hashHistory} from 'react-router';
 
+class Main extends React.Component {
+    render() {
+        return <h1>&lt;Main/&gt;!</h1>;
+    }
+}
+
+class Contact extends React.Component {
+    render() {
+        return <h1>Contact us at contact@example.com</h1>;
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return <Main />;
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', function(){
     ReactDOM.render(
-        <h1>Hello World</h1>,
+        <App/>,
         document.getElementById('app')
     );
 });
